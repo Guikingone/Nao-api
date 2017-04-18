bac8862146225eb774947439d848ae115daf7544
Guikingone <guillaume.loulier@hotmail.fr>
2017-04-18 19:05:45 +0200

    [ADD + FIX](Tests + Insight)[!M] Tests|Started + Insight|Progress

---
+++ tests/AppBundle/Entity/ObservationsTest.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the Nao-API project.
+ *
+ * (c) Guillaume Loulier <contact@guillaumeloulier.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tests\AppBundle\Entity;
+
+use AppBundle\Entity\Observations;
+use Symfony\Bundle\FrameworkBundle\Tests\TestCase;
+
+/**
+ * Class ObservationsTest
+ *
+ * @author Guillaume Loulier <contact@guillaumeloulier.fr>
+ */
+class ObservationsTest extends TestCase
+{
+    /**
+     * Test if a new entity can be created from the main Entity.
+     */
+    public function testObservationsIsCreated()
+    {
+
+    }
+}
